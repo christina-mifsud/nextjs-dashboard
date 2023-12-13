9429b8de4aed3f840bea12cca05e3ac5625eb3a9
christina-mifsud <christina.mifsud@gmail.com>
2023-12-13 23:24:13 +0000

    Initial commit
    Chap 5/16

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,9 @@
+//  Layout - to create UI that is shared between multiple pages.
+//  This is the root layout (found in the root of the project) - required.Any UI added to the root layout will be shared across all pages in the app.Here you can modify your < html > and < body > tags, and add metadata.
+
+import '@/app/ui/global.css';
+import { inter } from '@/app/ui/fonts';
+
 export default function RootLayout({
   children,
 }: {
@@ -5,7 +11,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body className={`${inter.className} antialiased`}>{children}</body>
     </html>
   );
 }
